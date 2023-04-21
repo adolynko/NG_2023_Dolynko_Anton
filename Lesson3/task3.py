6ff3c672505b219c130051d66e3e225d180a39b4
adolynko <dolynkoanton@gmail.com>
2023-04-21 21:46:05 +0300

    task3 done

---
+++ Lesson3/task3.py
@@ -0,0 +1,58 @@
+library = {}
+
+def addBook():
+    global name 
+    name = input("input name : ")
+    author = input("input author : ")
+    numberOfPages = input("input number of pages : ")
+    genre = input("input genre : ")
+    binding = input("input binding : ")
+    myName = {name:{"author": author, "pages": numberOfPages,"genre": genre, "binding": binding}}
+
+    library.update(myName)
+    print(library)
+
+def editBook():
+    edit = input("choose item for editing: name, author, number of pages, genre, binding : ")
+    changes = input("input changes : ").lower()
+    library[name][edit] = changes
+    print(library)
+
+def findBookName():
+    bookName = input("input found book : ")
+    for value in library:
+        if value == bookName:
+            print("book is exist")
+
+def start():
+    while True:
+        choose = input(
+"""input command :
+1 - add book
+2 - remove book
+3 - edit book
+4 - find book
+5 - exit
+""")
+        match choose:
+            case("1"):
+                print("you must add name, author, number of pages, genre, binding (hard/soft)")
+                addBook()
+            case("2"):
+                print("enter book name for deleting : ")
+                try:
+                    name = input()
+                    library.pop(name)
+                    print(library)
+                except:
+                    print("book doesn`t exist")
+            case('3'):
+                editBook()
+            case("4"):
+                findBookName()
+            case("5"):
+                exit()
+            case(_):
+                print("Wrong command")
+
+start()
