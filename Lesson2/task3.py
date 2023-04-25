cd71bc4e0301ad17c79279f2c9e700fa7a065eff
adolynko <dolynkoanton@gmail.com>
2023-04-25 19:59:41 +0300

    script takes 255 elements now

--- Lesson2/task3.py
+++ Lesson2/task3.py
@@ -1,11 +1,19 @@
-firstList = input().split(' ')
-secondList = input().split(' ')
-thirdList = input().split(' ')
-temp = firstList,secondList,thirdList
+lists = [list([str(k) for k in range(10)]) for n in range(255)]
+print("number of lists :",len(lists))
+set_list = ""
+for i_item in lists:
+    for j_item in i_item:
+        set_list += j_item
+    #print(set_list)
+print(set(set_list))
+#print(lists)
+
+"""temp = firstList,secondList,thirdList
 temp = list(temp)
 result = ""
 for item in range(len(temp)):
     for values in temp[item]:
         result += values
         print(values)
-print(set(result))
+print(set(result))"""
+
