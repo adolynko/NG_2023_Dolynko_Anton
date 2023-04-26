b4f2046d71c262e9885ada2aaad114ad46070698
adolynko <dolynkoanton@gmail.com>
2023-04-26 23:39:43 +0300

    more readble code

--- Lesson3/task4.py
+++ Lesson3/task4.py
@@ -2,8 +2,12 @@ import requests
 
 site_list = ["https://www.w3schools.com/ai/ai_tensorflow_intro.asp","https://www.chess.com/game/live/75653394137","https://jut.su/nanatsu-taizai/season-2/episode-3.html","https://www.youtube.com/watch?v=jvFULnNpNLg&ab_channel=egoroff_channel","https://www.dota2.com/newfrontiers?v=2"]
 
-status = [f"{link}, is ok" if requests.get(link).status_code == 200 or requests.get(link).status_code == 302 else f"{link} , is fail" for link in site_list]
-[print(item) for item in status]
+for link in site_list:
+    status = requests.get(link).status_code
+    if status == 200 or status == 302:
+        print(f"'{link}' link is ok!")
+    else:
+        print(f"'{link}' link isn`t work :( ")
 
 
 
