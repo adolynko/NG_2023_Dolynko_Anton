d45792a146e679b9c6d3de9a9e91ba974b20de0e
adolynko <dolynkoanton@gmail.com>
2023-04-26 23:41:03 +0300

    now you can create too much lists

--- Lesson2/task3.py
+++ Lesson2/task3.py
@@ -1,19 +1,8 @@
-lists = [list([str(k) for k in range(10)]) for n in range(255)]
+size = int(input("input wish size of list : "))
+lists = [list([str(item) for item in range(10)]) for n in range(size)]
 print("number of lists :",len(lists))
 set_list = ""
 for i_item in lists:
     for j_item in i_item:
         set_list += j_item
-    #print(set_list)
 print(set(set_list))
-#print(lists)
-
-"""temp = firstList,secondList,thirdList
-temp = list(temp)
-result = ""
-for item in range(len(temp)):
-    for values in temp[item]:
-        result += values
-        print(values)
-print(set(result))"""
-
