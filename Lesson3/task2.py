f0d414b958b27f8a998797cb7f178cf55de5871d
adolynko <dolynkoanton@gmail.com>
2023-04-26 23:42:06 +0300

    added stars at top and bottom

--- Lesson3/task2.py
+++ Lesson3/task2.py
@@ -1,10 +1,12 @@
-size = int(input("input size"))
-gap = 0
+size = int(input("input size : "))
+gap = 1
 
 def cube(size,gap):
-    if gap < size:
-        print(size * " " + "*"+ gap * 2 * " " + "*" + size * " ")
-        cube(size - 1, gap + 1)
-        print(size * " " + "*"+ gap * 2 * " " + "*" + size * " ")   
+    if size != 1:
+        print(size * " " + gap * "*" + size * " ")
+        cube(size - 1, gap + 2)
+        print(size * " " + gap * "*" + size * " ")
+    
 
 cube(size, gap)
+
